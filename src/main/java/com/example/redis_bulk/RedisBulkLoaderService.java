115f5b05261de09262b07762e42366e8218b38fb
Sasi Raj <sasirajp18@gmail.com>
2024-07-05 21:46:52 +0530

    added comment

--- src/main/java/com/example/redis_bulk/RedisBulkLoaderService.java
+++ src/main/java/com/example/redis_bulk/RedisBulkLoaderService.java
@@ -14,9 +14,14 @@ public class RedisBulkLoaderService {
     public void bulkLoadService(String filePath) throws IOException, InterruptedException {
 
         log.info("Bulk load in redis process started");
+//        For docker redis
         ProcessBuilder processBuilder = new ProcessBuilder(
                 "docker", "exec", "-i", "docker-compose-redis-1", "redis-cli", "--pipe"
         );
+//        For redis installed in host
+//        ProcessBuilder processBuilder = new ProcessBuilder(
+//                "redis-cli", "--pipe"
+//        );
         processBuilder.redirectInput(new java.io.File(filePath));
         Process process = processBuilder.start();
         String s;
